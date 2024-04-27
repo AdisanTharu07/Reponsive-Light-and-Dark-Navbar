82a4c73ef210448aa3195d9083c44b6d5cb4174b
Adisan Tharu <cryuscode011@gmail.com>
2024-04-27 10:48:23 +0545

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// dark and light code
+function darkBtn() {
+    const toggle = document.body;
+    toggle.classList.toggle('darkmode');
+}
+
+// open nav code
+var showMenu = document.getElementById('showMenu');
+function menuBtn() {
+    showMenu.classList.toggle('open__nav');
+}
